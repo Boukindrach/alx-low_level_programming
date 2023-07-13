18be465dadfece73b23b17cbb2b02db3d59cbae0
Boukind Rachid <boukind.r1@gmail.com>
2023-07-13 16:34:12 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -it prints out output using printf function
+ *
+ * Return: always (0)
+ *
+*/
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
