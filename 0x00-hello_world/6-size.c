18823a88bbe777d736d6e2a988089db6e39719e0
Boukind Rachid <boukind.r1@gmail.com>
2023-07-13 17:00:20 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 /**
  * main -Entry Point
  *
- * description: using sizeof to print the size various types.
+ * Description: using sizeof to print the size various types.
  *
- * return: always (0)
+ * Return: always 0
 */
 
 int main(void)
