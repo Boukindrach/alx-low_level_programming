bb75b0423150a1fabc63663c4309e9fe0bc20fa1
Boukind Rachid <boukind.r1@gmail.com>
2023-07-16 20:40:34 -0700

    the quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
-/*
- *main -entery point
+/**
+ * main - Entry point
  *
- *return: it's 1 this time
+ * return: it's 1 this time
 */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, quote, sizeof(quote));
+
 	return (1);
 }
