1125a9d101b923b7244ca81a9032b503c6092ae2
Boukind Rachid <boukind.r1@gmail.com>
2023-07-18 16:38:10 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for lowcase and 0 for else
+*/
+
+int _isalpha(int c)
+{
+	if (c < 99 && c > 99)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
