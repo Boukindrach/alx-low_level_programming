853b73ab7d559bedf8829fc55eff8d213493d99b
Boukind Rachid <boukind.r1@gmail.com>
2023-07-18 19:44:55 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - computes the absolute value of an integer
+*/
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		for (b = 0; b <= 9; b++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			c = a * b;
+			if (c <= 0)
+			
+				_putchar(' ');
+			
+			else 
+			
+				_putchar((c / 10) + '0');
+
+			_putchar((c % 10) + '0');
+			
+		}
+		_putchar('\n');
+	}
+}
