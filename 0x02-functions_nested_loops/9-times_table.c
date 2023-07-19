0fbae4d1fe63ef7bb32265fe0b80d0eb7771437e
Boukind Rachid <boukind.r1@gmail.com>
2023-07-18 20:02:43 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,13 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0');
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			c = a * b;
-			if (c <= 0)
+			if (c <= 9)
 
 				_putchar(' ');
 
