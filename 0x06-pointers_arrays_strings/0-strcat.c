cc4da3c3f3a4380e7d353f4ed9717e7ff1f77c7c
Boukind Rachid <boukind.r1@gmail.com>
2023-07-27 09:31:08 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,8 +11,12 @@ char *_strcat(char *dest, char *src)
 {
 	int a;
 	int b;
-
-	for (a = 0; dest[a] != '\0'; a++)
+	
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
 
 	b = 0;
 	while (src[b] != '\0')
