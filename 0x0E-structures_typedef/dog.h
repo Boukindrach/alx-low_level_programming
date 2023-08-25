94b8111d1f76dbfb22684bed4c31461b46ddb12a
Boukind Rachid <boukind.r1@gmail.com>
2023-08-24 19:12:03 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,10 @@
 #ifndef DOG_H
-#define DOG_h
+#define DOG_H
 
-/**
-* @name: pointer to the name of the dog
-* @age: age of dog
-* @owner: pointer to owner of dog
-*/
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
+struct dog {
+    char *name;
+    float age;
+    char *owner;
 };
 
-#endif
+#endif /* DOG_H */
