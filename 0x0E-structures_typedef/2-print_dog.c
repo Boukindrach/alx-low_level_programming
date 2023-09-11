925ddd44e4207cc3fea3a3266a1c3bea50b6e3de
Boukind Rachid <boukind.r1@gmail.com>
2023-09-11 13:20:51 -0700

    C

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d->name)
+	if (d->name == NULL)
 		d->name = "(nil)";
-	if (d->age)
-		d->age = "(nil)";
-	if (d->owner)
+	if (d->owner == NULL)
 		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
