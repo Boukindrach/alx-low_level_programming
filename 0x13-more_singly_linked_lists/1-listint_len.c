edd1850998c1438642c2fd44016a69a529c55372
Boukind Rachid <boukind.r1@gmail.com>
2023-09-25 03:44:49 -0700

    B

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: A pointer to the head of a linked list of type listint_t
+ * Return: The total number of nodes in the linked list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	for (; h; h = h->next, count++)
+		;
+
+	return count;
+}
