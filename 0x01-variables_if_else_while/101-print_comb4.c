30d1ab147ba8ae49055068222cf63933e5fae2d6
Boukind Rachid <boukind.r1@gmail.com>
2023-11-01 06:03:12 -0700

    Back to do some C

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int i, j, z;
+
+	for (i = 0; i <= 7; i++)
+		for (j = 1; j <= 8; j++)
+			for (z = 2; z <= 9; z++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + z);
+				if (i != 7 || j != 8 || z != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	putchar('\n');
+	return (0);
+}
