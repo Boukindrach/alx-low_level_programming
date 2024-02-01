f582150ac6c1a3383b7b7cedbfa4913c1c8d8e15
Rachid Boukind <boukind.r1@gmail.com>
2024-02-01 06:12:14 -0800

    A

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of the array
+ *
+ * Return: If an error occurs does NULL.
+ * Else a pointer to the new hash table
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+	unsigned long int i;
+
+	hash = malloc(sizeof(hash_table_t));
+	if (hash == NULL)
+		return (NULL);
+
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hash->array[i] = NULL;
+
+	return (hash);
+}
