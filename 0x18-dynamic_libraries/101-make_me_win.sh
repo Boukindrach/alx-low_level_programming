1f1e50415bbdd4ada4c984024c3bd23a3f138f02
Boukind Rachid <boukind.r1@gmail.com>
2024-03-30 12:30:22 -0700

    a

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/alx-tools/0x18.c/blob/7869708b060ff8a15c66e8ec6b063b108ed7e5d5/101-md5_gm -O 101-md5_gm
-
-if [ $? -eq 0 ]; then
-    echo "Download successful."
-else
-    echo "Failed to download gm from GitHub."
-    exit 1
-fi
-
-chmod u+x 101-md5_gm
-
+wget https://github.com/alx-tools/0x18.c/blob/7869708b060ff8a15c66e8ec6b063b108ed7e5d5/gm -O gm
+chmod +x gm
 export LD_PRELOAD=./101-make_me_win.so
-
-./101-md5_gm 9 8 10 24 75 9
