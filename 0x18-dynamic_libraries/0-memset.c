2b63e50f907716f8e8c785d1a1534f84d7d289b7
Boukind Rachid <boukind.r1@gmail.com>
2024-03-30 11:43:37 -0700

    a

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset-  fills memory with a constant byte.
+ * @s: pointer
+ * @b: char
+ * @n: number
+ * Return: pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *x = s;
+
+	if (s == 0)
+		return (s);
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (x);
+}
