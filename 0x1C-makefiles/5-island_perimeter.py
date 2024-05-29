54cef323cb1f39e302f2cf86597cb8850edd59e3
Boukind Rachid <boukind.r1@gmail.com>
2024-05-29 16:19:30 -0700

    a

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island represented in a grid.
+
+    The grid contains water represented by 0 and land represented by 1.
+
+    Args:
+        grid: A grid where 0 represents water and 1 represents land.
+
+    Returns:
+        int: The perimeter of the island defined in the grid.
+    """
+
+    height = len(grid)
+    width = len(grid[0])
+
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    edges += 1
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    edges += 1
+    return size * 4 - edges * 2
